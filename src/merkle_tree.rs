4df7fb89c3ca66f5e173b061fde8f705aaf2a17b
SerxaS <serhas@gmail.com>
2023-09-14 17:37:26 +0300

    Made some typo corrections.

--- src/merkle_tree.rs
+++ src/merkle_tree.rs
@@ -1,12 +1,12 @@
 #[cfg(test)]
 mod tests {
-    use crate::tree::build_tree::{build_tree, hashed_leafs, leaf, to_hex_string};
+    use crate::tree::build_tree::{build_tree, hashed_leaves, leaf, to_hex_string};
 
     #[test]
     fn test() {
         let number_of_leaf = 8;
-        let leafs = hashed_leafs(leaf(number_of_leaf));
-        println!("Leafs:{:?}", to_hex_string(leafs.clone()));
-        build_tree(leafs);
+        let leaves = hashed_leaves(leaf(number_of_leaf));
+        println!("Leaves:{:?}", to_hex_string(leaves.clone()));
+        build_tree(leaves);
     }
 }
